050c7ad556931807b2a0ab63a4dd8750dd1a2603
Garden <nogamegarden@gmail.com>
2026-06-18 02:56:27 -0300

    feat: adicionado animação hover no texto das categorias

--- 02_Source/ProjetoECommerce/src/pages/Home.jsx
+++ 02_Source/ProjetoECommerce/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import "../styles/Home.css"
@@ -11,28 +12,28 @@ export function Home() {
                     <h2 className="page-body-classes-title">CATEGORIAS</h2>
                     <div className="page-body-classes-cards">
                         <div className="page-body-classes-playstation">
-                            <div className="page-body-classes-playstation-gradient">
+                            <Link to="/playstation" className="page-body-classes-playstation-gradient">
                                 <img src="Img-Sony-PlayStation-PNG-Image 1.png" alt="" className="page-body-classes-playstation-gradient-img" />
                                 <h3 className="page-body-classes-playstation-gradient-title">PlayStation</h3>
-                            </div>
+                            </Link>
                         </div>
                         <div className="page-body-classes-xbox">
-                            <div className="page-body-classes-xbox-gradient">
+                            <Link to="/xbox" className="page-body-classes-xbox-gradient">
                                 <img src="Img-Xbox.png" alt="" className="page-body-classes-xbox-gradient-img" />
                                 <h3 className="page-body-classes-xbox-gradient-title">Xbox</h3>
-                            </div>
+                            </Link>
                         </div>
                         <div className="page-body-classes-nintendo">
-                            <div className="page-body-classes-nintendo-gradient">
+                            <Link to="/nintendo" className="page-body-classes-nintendo-gradient">
                                 <img src="5bbc07b9af4bb-5135506e4e5927d3c9fc6b95a3322b97 1.png" alt="" className="page-body-classes-nintendo-gradient-img" />
                                 <h3 className="page-body-classes-nintendo-gradient-title">Nintendo</h3>
-                            </div>
+                            </Link>
                         </div>
                         <div className="page-body-classes-retro">
-                            <div className="page-body-classes-retro-gradient">
+                            <Link to="/retro" className="page-body-classes-retro-gradient">
                                 <img src="563-5632966_sega-konsolen-liste-sega-mega-drive-classic-hd-Photoroom 1.png" alt="" className="page-body-classes-retro-gradient-img" />
                                 <h3 className="page-body-classes-retro-gradient-title">Retro</h3>
-                            </div>
+                            </Link>
                         </div>
                     </div>
                 </section>
